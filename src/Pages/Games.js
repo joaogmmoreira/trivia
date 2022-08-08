0be2ec627c58f22738a2db7131eecfe67432a5c7
Joao Moreira <jgmoreira1809@gmail.com>
2022-08-08 13:44:06 -0700

    Requisito 6

--- src/Pages/Games.js
+++ src/Pages/Games.js
@@ -1,12 +1,45 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import Header from '../Components/Header';
+import fetchQuestions from '../Services/fetchQuestions';
 
 class Games extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+    this.getQuestions();
+  }
+
+  getQuestions = async () => {
+    const { history } = this.props;
+    const questions = await fetchQuestions();
+    console.log(questions);
+
+    if (questions.response_code !== 0) {
+      // console.log('oi');
+      localStorage.removeItem('token');
+      history.push('/');
+    }
+    return questions.results;
+  }
+
   render() {
     return (
       <Header />
     );
   }
 }
+
+Games.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
 
 export default Games;
