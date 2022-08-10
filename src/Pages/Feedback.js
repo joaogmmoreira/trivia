1130e484660728af3d93adbd9501ee62daee419c
Fillipe Oliveira <fillipe.trybe@gmail.com>
2022-08-10 19:02:08 -0300

    Requisito 15

--- src/Pages/Feedback.js
+++ src/Pages/Feedback.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import HeaderFeedback from '../Components/HeaderFeedback';
+import PlayAgain from '../Components/PlayAgain';
 
 class Feedback extends React.Component {
   render() {
+    const { history } = this.props;
     return (
       <div>
         <HeaderFeedback />
+        <PlayAgain history={ history } />
       </div>
     );
   }
 }
+
+Feedback.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
 
 export default Feedback;
