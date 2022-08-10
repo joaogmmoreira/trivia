9f562e3830d13eb356689007a43c4fd72629ac21
Gabriela Delamare <gabrieladnr@gmail.com>
2022-08-10 17:56:26 -0300

    req 13 pronto

--- src/Pages/Feedback.js
+++ src/Pages/Feedback.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Feedback extends React.Component {
+  feedbackMessage() {
+    const { assertions } = this.props;
+    const three = 3;
+    if (assertions < three) {
+      return <h2>Could be better...</h2>;
+    }
+    if (assertions >= three) {
+      return <h2>Well Done!</h2>;
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>Feedback</h1>
+        { this.feedbackMessage() }
       </div>
     );
   }
 }
+const mapStateToProps = ({ player }) => ({
+  assertions: player.assertions,
+});
 
-export default Feedback;
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Feedback);
